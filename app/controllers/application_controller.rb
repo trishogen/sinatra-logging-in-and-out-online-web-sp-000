95a98fbe9432b64000c748a95c6465e554fdd162
Trish Ogen <patriciaogen@gmail.com>
2019-08-13 00:39:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by!(username: params[:username])
     session[:user_id] = @user.id
     if Helpers.is_logged_in?(session)
       redirect to "/account"
