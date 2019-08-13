4591da52865988ada7922970bb8ad3647c4398bf
Trish Ogen <patriciaogen@gmail.com>
2019-08-13 00:20:09 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,7 @@
 class Helpers
 
   def self.current_user(session)
-    puts session[:user_id]
-    @user = User.find(session['user_id'])
+    @user = User.find(session[:user_id])
   end
 
   def self.is_logged_in?
